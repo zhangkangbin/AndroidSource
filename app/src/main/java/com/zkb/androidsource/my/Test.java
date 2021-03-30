42473efa012f5a4a6a56c6c5353e3a50ad94f1e7
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2021-03-30 15:30:01 +0800

    improve our quality of life

---
+++ app/src/main/java/com/zkb/androidsource/my/Test.java
@@ -0,0 +1,30 @@
+package com.zkb.androidsource.my;
+
+/**
+ * save money and save time.
+ * it can save time for us.
+ * improve our quality of life.
+ * free time,自由时间
+ * I don't have to work overtime.
+ *
+ * I can enjoy much freedom.
+ *
+ * The job pays well.
+ *
+ * help us relax.
+ *
+ * reduce our pressure.
+ *
+ * keep healthy.
+ *
+ * is good for us.
+ *
+ * is very important
+ * is very popular.
+ * it is good for the environment
+ *
+ * make the world a better place.
+ * my job can make the world better place.
+ */
+public class Test {
+}
